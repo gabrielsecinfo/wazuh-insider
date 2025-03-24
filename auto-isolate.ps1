b8f6ea075aef214410102edccde996955f5e46b7
Gabriel Ribeiro <73762991+gabrielsecinfo@users.noreply.github.com>
2025-03-24 00:08:12 -0300

    Add files via upload

---
+++ auto-isolate.ps1
@@ -0,0 +1,5 @@
+﻿$IP_atacante = "192.168.18.100"
+# Bloqueia o tráfego de entrada e saída do IP malicioso
+New-NetFirewallRule -DisplayName "Bloqueio IP Malicioso" -Direction Inbound -Action Block -RemoteAddress $IP_atacante -Enabled True
+New-NetFirewallRule -DisplayName "Bloqueio IP Malicioso" -Direction Outbound -Action Block -RemoteAddress $IP_atacante -Enabled True
+Write-Host "IP $IP_atacante foi isolado"
